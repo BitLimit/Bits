6be0da20c6fdf8293451a4049234f8e7f659f163
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-10-21 23:09:16 -0700

    Update

---
+++ src/main/java/com/bitlimit/bits/bukkit/BitsPlugin.java
@@ -0,0 +1,18 @@
+package com.bitlimit.bits.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BitsPlugin extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+}
