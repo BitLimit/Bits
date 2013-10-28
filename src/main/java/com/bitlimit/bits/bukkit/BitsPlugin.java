c6ae02eb2aa707254f5adb774f61de53af3aaabe
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-10-27 21:12:49 -0700

    Add database conncetion.

--- src/main/java/com/bitlimit/bits/bukkit/BitsPlugin.java
+++ src/main/java/com/bitlimit/bits/bukkit/BitsPlugin.java
@@ -1,13 +1,22 @@
 package com.bitlimit.bits.bukkit;
 
+import com.bitlimit.pulse.PluginCondition;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.bitlimit.pulse.Pulse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+
 
 public class BitsPlugin extends JavaPlugin
 {
 	@Override
 	public void onEnable()
 	{
+		Pulse.notePluginCondition(this, PluginCondition.LOADED, true, null);
 
+		this.initializeConnection();
 	}
 
 	@Override
@@ -15,4 +24,26 @@ public class BitsPlugin extends JavaPlugin
 	{
 
 	}
+
+	public void initializeConnection()
+	{
+		Integer port = 5432;
+		String username = "yolo";
+		String password = "swag";
+
+		try
+		{
+			Pulse.recordCondition(this, "connecting to database.", Level.FINEST);
+
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:" + port.toString() + "/bits", username, password);
+			connection.close();
+
+			Pulse.recordCondition(this, "connected to database.", Level.FINE);
+		}
+		catch (Exception e)
+		{
+			Pulse.recordCondition(this, "failed to connect to database.", Level.SEVERE);
+		}
+	}
+
 }
