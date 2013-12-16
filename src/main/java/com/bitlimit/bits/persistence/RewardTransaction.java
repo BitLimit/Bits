6ca89aaa3a8ec41f16825b9abae3e8292bfea17d
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-12-15 16:07:48 -0800

    Add two base model objects, accounts and reward transactions.

---
+++ src/main/java/com/bitlimit/bits/persistence/RewardTransaction.java
@@ -0,0 +1,14 @@
+package com.bitlimit.bits.persistence;
+
+import org.javalite.activejdbc.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kolin
+ * Date: 12/15/13
+ * Time: 3:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RewardTransaction extends Model
+{
+}
