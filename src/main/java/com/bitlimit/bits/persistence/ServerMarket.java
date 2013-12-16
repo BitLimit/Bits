2ca1fb1d62a1f28b3f1fac2fd6637fd0c9146e1d
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-12-15 16:11:32 -0800

    Add server market class.

---
+++ src/main/java/com/bitlimit/bits/persistence/ServerMarket.java
@@ -0,0 +1,14 @@
+package com.bitlimit.bits.persistence;
+
+import org.javalite.activejdbc.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kolin
+ * Date: 12/15/13
+ * Time: 4:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerMarket extends Model
+{
+}
