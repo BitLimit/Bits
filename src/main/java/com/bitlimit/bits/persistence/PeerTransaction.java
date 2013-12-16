b47233910f87891163d03368ac5660afb0068890
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-12-15 16:09:41 -0800

    Add PeerTransaction class.

---
+++ src/main/java/com/bitlimit/bits/persistence/PeerTransaction.java
@@ -0,0 +1,14 @@
+package com.bitlimit.bits.persistence;
+
+import org.javalite.activejdbc.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kolin
+ * Date: 12/15/13
+ * Time: 4:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PeerTransaction extends Model
+{
+}
